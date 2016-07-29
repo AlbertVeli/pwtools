fecc52f9a9abb1c7a3de6beabb9a0f620465947d
Albert Veli <albert.veli@gmail.com>
2016-07-29 14:42:13 +0200

    Add some comment documentation for unhexhashcat.

--- unhexhashcat.py
+++ unhexhashcat.py
@@ -2,6 +2,27 @@
 
 # Unhex hex-lines from hashcat.pot
 #
+# To speed up execution for large files you might first
+# extract the $HEX[ lines with grep and then unhex them:
+#
+# grep '\$HEX\[' hashcat.pot > hex.txt
+# unhexhashcat.py hex.txt > words.txt
+#
+# Grep is much faster than both bython and sed for large files,
+# that is why the two commands above are faster than running
+# unhexhashcat.py on the whole potfile. Then the rest of the
+# words can be extracted from the potfile
+# like this:
+#
+# grep -v '\$HEX\[' hashcat.pot | cut -c 42- > nohexwords.txt
+#
+# where 42 is the number of characters to cut off the beginning. It
+# depends on the hash type. 42 for sha1, 34 for md5 etc.
+#
+# Replace cut with: sed 's/^[^:]*://'
+# for a general, but slower solution.
+#
+#
 # All Rights Reversed
 # Albert Veli, Setting Orange, the 64th day of Confusion in the YOLD 3182
 
